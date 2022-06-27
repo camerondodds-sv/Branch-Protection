a90ac20b558c35221d5f80e87dd358a9ee7cefbe
Cameron Dodds <camerondodds@smartvisionworks.com>
2022-06-27 12:46:22 -0600

    added get branch code to main

--- branch_protection.py
+++ branch_protection.py
@@ -1,1 +1,9 @@
-
+# main branch
+import requests 
+url = "https://api.github.com/repos/camerondodds-sv/Branch-Protection/branches"
+token = "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"
+print("token {}".format(token))
+headers = {"Authorization" : "token {}".format(token)} 
+response = requests.get(url, headers=headers)
+ 
+print(response.json())
